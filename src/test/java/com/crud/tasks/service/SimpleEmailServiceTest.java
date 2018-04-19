15020e07d8e7fc76b55122b5a448ab4799d7ee3e
rundna <run.dna@gmail.com>
2018-04-19 20:16:05 +0200

    mail

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,37 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail(){
+        Mail mail = new Mail("test@test.com","Test","Test message","");
+
+        SimpleMailMessage mailMassage = new SimpleMailMessage();
+        mailMassage.setTo(mail.getMailTo());
+        mailMassage.setSubject(mail.getSubject());
+        mailMassage.setText(mail.getMessage());
+
+        simpleEmailService.send(mail);
+
+        verify(javaMailSender,times(1)).send(mailMassage);
+    }
+
+}
