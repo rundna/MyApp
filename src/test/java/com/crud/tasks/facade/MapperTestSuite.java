5e20653a57c739d5d37fcf9aec845cc9b1cdcdd8
rundna <run.dna@gmail.com>
2018-05-03 08:24:54 +0200

    update mapper tests

--- src/test/java/com/crud/tasks/facade/MapperTestSuite.java
+++ src/test/java/com/crud/tasks/facade/MapperTestSuite.java
@@ -1,38 +1,15 @@
 package com.crud.tasks.facade;
 
 import com.crud.tasks.domain.*;
-import com.crud.tasks.mapper.CreatedTrelloCardDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.mapper.TrelloMapper;
-import com.crud.tasks.service.TrelloService;
-import com.crud.tasks.trello.client.TrelloClient;
-import com.crud.tasks.trello.config.TrelloConfig;
-import com.crud.tasks.trello.facade.TrelloFacade;
-import com.crud.tasks.trello.validator.TrelloValidator;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MapperTestSuite {
