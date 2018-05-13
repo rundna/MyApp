e1ee2dfd2dcd1f63fb0fc991ba6dd5a308822d52
rundna <run.dna@gmail.com>
2018-05-13 12:29:43 +0200

    facebook birthady

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -13,26 +13,10 @@ public class FacebookTestingApp {
     public static final String XPATH_BIRTHYEAR = "//div[contains(@class,\"_5k_5\")]/span/span/select[3]";
 
 
-    public static final String XPATH_SELECT1 = "//select[1]";
-    public static final String XPATH_SELECT2 = "//select[2]";
-    public static final String XPATH_SELECT3 = "//select[3]";
-
-    public static final String XPATH_ADD_TASK = "//html/body/main/section/form/fieldset[3]/button";
-
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com");
 
-        //WebElement searchField = driver.findElement(By.xpath(XPATH_INPUT));
-        //searchField.sendKeys("New robotic task");
-
-        //WebElement textareaField = driver.findElement(By.xpath(XPATH_TEXTAREA));
-        //textareaField.sendKeys("Robotic content2");
-
-       // WebElement birthDay = driver.findElement(By.xpath(XPATH_BIRTHDAY));
-        //birthDay.click();
-
-        //while (!driver.findElement(By.xpath(XPATH_BIRTHDAY)).isDisplayed()) ;
         WebElement selectDay=driver.findElement(By.xpath(XPATH_BIRTHDAY));
         WebElement selectMonth=driver.findElement(By.xpath(XPATH_BIRTHMONTH));
         WebElement selectYear=driver.findElement(By.xpath(XPATH_BIRTHYEAR));
@@ -40,10 +24,10 @@ public class FacebookTestingApp {
         selectFDay.selectByValue("20");
         Select selectFMonth = new Select(selectMonth);
         selectFMonth.selectByIndex(6);
-        //selectMonth.submit();
+
         Select selectFYear = new Select(selectYear);
         selectFYear.selectByValue("2006");
-        //selectYear.submit();
+
 
 
     }
